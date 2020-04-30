ce668be4d4b91dcbe98896cfb6160602784549f6
Joseph Liu <josephliu@Josephs-MacBook-Pro.local>
2020-04-30 18:35:50 +0800

    Fix EvaluateModel

--- bert/utils.py
+++ bert/utils.py
@@ -1,6 +1,5 @@
-import time
-import datetime
 import os
+import numpy as np
 
 from config import PATH
 
@@ -43,13 +42,3 @@ def accuracy_score(preds, labels):
     labels_flat = labels.flatten()
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
 
-
-def format_time(elapsed):
-    '''
-    Takes a time in seconds and returns a string hh:mm:ss
-    '''
-    # Round to the nearest second.
-    elapsed_rounded = int(round((elapsed)))
-    
-    # Format as hh:mm:ss
-    return str(datetime.timedelta(seconds=elapsed_rounded))
