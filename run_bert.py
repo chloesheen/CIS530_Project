330d18d67cc7bd78288785ed170d67eef434baba
Joseph Liu <josephliu@Josephs-MacBook-Pro.local>
2020-05-01 20:35:00 +0800

    Add in-memory dataset class

--- run_bert.py
+++ run_bert.py
@@ -4,7 +4,7 @@ BERT model for author identification.
 import argparse
 import math
 import torch
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from transformers import (
     AdamW,
     BertConfig,
@@ -44,7 +44,7 @@ from bert.callbacks import *
 from bert.datasets import Reuters50Dataset
 from bert.train import fit, predict_episode
 from bert.utils import setup_dirs
-from config import PATH
+from config import PATH, DATA_PATH
 
 setup_dirs()
 if torch.cuda.is_available():
@@ -94,19 +94,59 @@ tokenizer = tokenizer_class.from_pretrained(args.model_name,
 ###################
 # Create datasets #
 ###################
-train_dataset = Reuters50Dataset('train', 
-                                 args.model_type, 
-                                 tokenizer,
-                                 args.max_length)
+def get_encodings(texts):
+    token_ids = []
+    attention_masks = []
+    for text in texts:
+        token_id = tokenizer.encode(text, 
+                                    add_special_tokens=True, 
+                                    max_length=args.max_length,
+                                    pad_to_max_length=True)
+        token_ids.append(token_id)
+    return token_ids
+
+
+def get_attention_masks(padded_encodings):
+    attention_masks = []
+    for encoding in padded_encodings:
+        attention_mask = [int(token_id > 0) for token_id in encoding]
+        attention_masks.append(attention_mask)
+    return attention_masks
+
+train_df = pd.read_pickle(ROOT + '/reuters50_train.pkl')
+test_df = pd.read_pickle(ROOT + '/reuters50_test.pkl')
+
+train_encodings = get_encodings(train_df.text.values)
+train_attention_masks = get_attention_masks(train_encodings)
+
+test_encodings = get_encodings(test_df.text.values)
+test_attention_masks = get_attention_masks(test_encodings)
+
+train_input_ids = torch.tensor(train_encodings)
+train_masks = torch.tensor(train_attention_masks)
+train_labels = torch.tensor(train_df.author_id.values)
+
+
+test_input_ids = torch.tensor(test_encodings)
+test_masks = torch.tensor(test_attention_masks)
+test_labels = torch.tensor(test_df.author_id.values)
+
+
+train_data = TensorDataset(train_input_ids, train_masks, train_labels)
+# train_dataset = Reuters50Dataset('train', 
+#                                  args.model_type, 
+#                                  tokenizer,
+#                                  args.max_length)
 train_sampler = RandomSampler(train_dataset)
 train_dataloader = DataLoader(train_dataset, 
                               sampler=train_sampler,
                               batch_size=batch_size)
 
-test_dataset = Reuters50Dataset('test',
-                                args.model_type,
-                                tokenizer,
-                                args.max_length)
+validation_data = TensorDataset(test_input_ids, test_masks, test_labels)
+# test_dataset = Reuters50Dataset('test',
+#                                 args.model_type,
+#                                 tokenizer,
+#                                 args.max_length)
 test_sampler = SequentialSampler(test_dataset)
 test_dataloader = DataLoader(test_dataset,
                              sampler=test_sampler,
